75b00e4e09e6eb4a3b7dd4b338042b6a43f4622e
Daniel Muehlbachler-Pietrzykowski <daniel@muehlbachler.io>
2024-08-05 14:37:10 +0200

    feat: initial commit

---
+++ src/main/java/io/muehlbachler/bswe/StringService.java
@@ -0,0 +1,13 @@
+package io.muehlbachler.bswe;
+
+import org.springframework.stereotype.Service;
+
+// FIXME: add javadoc
+@Service
+public class StringService {
+    // FIXME: add javadoc
+    public String toUpperCase(final String string) {
+        // FIXME: implement
+        return string;
+    }
+}
